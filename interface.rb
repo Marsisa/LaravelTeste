7a40f73c633f55662f9ea135fc6cc8892539a3bc
Marsisa <95244379+Marsisa@users.noreply.github.com>
2020-05-30 15:34:34 +0000

    Automatically update dependencies

--- interface.rb
+++ interface.rb
@@ -9,3 +9,11 @@ data = DataPreparation.new(Record)
 output = data.execute("./sample.txt")
 analysis = DataAnalysis.new(data)
 analysis.add_ranks
+
+# Top 15 for the three key attributes
+analysis.rank("top", "cloud_ceiling", "./top_cloud_ceiling.csv")
+analysis.rank("top", "visibility", "./top_visibility.csv")
+analysis.rank("top", "wind_speed", "./top_wind_speed.csv")
+analysis.rank_by_overall_score("top", "./top_overall_ranking.csv")
+
+# Bottom 15 for the three key attributes
