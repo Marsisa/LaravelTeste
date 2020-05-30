7916ffc1b08e169ee88db7798e8d9bacd83cae47
Marsisa <95244379+Marsisa@users.noreply.github.com>
2020-05-30 14:38:29 +0000

    Yet another fix

--- interface.rb
+++ interface.rb
@@ -4,3 +4,8 @@ require './lib/record.rb'
 require './lib/data_analysis.rb'
 
 
+
+data = DataPreparation.new(Record)
+output = data.execute("./sample.txt")
+analysis = DataAnalysis.new(data)
+analysis.add_ranks
