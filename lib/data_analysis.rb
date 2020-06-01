bedbee3cc656958eb0ac4ebf12a97b746c2fefa5
Marsisa <95244379+Marsisa@users.noreply.github.com>
2020-06-01 13:23:38 +0000

    Organization

--- lib/data_analysis.rb
+++ lib/data_analysis.rb
@@ -7,3 +7,10 @@ class DataAnalysis
     @data = data.output
     @sorted = []
   end
+
+  def add_ranks
+    add_cloud_rank
+    add_visibility_rank
+    add_wind_speed_rank
+    add_overall_score
+  end
