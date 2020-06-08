8d67e743486eb8df879cbd338e669adbf934071f
Marsisa <95244379+Marsisa@users.noreply.github.com>
2020-06-08 09:45:33 +0000

    Version bump to 1.3.1

--- lib/data_preparation.rb
+++ lib/data_preparation.rb
@@ -4,3 +4,13 @@ require './lib/record.rb'
 class DataPreparation
 
   def initialize(record_klass)
+    @record = record_klass
+    @output = {}
+  end
+
+  def output
+    @output
+  end
+
+  def execute(file)
+    File.readlines(file).each do |line|
