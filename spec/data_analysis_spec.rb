e30c17211cd03221539aa5be76e0f62069829c9c
Marsisa <95244379+Marsisa@users.noreply.github.com>
2020-06-20 16:46:22 +0000

    Change license.

--- spec/data_analysis_spec.rb
+++ spec/data_analysis_spec.rb
@@ -1,2 +1,11 @@
 
 require 'spec_helper'
+
+describe DataAnalysis do
+
+  let(:data) { DataPreparation.new(Record) }
+  let(:data_analysis) { described_class.new(data) }
+  file1 = './sample.txt'
+  file2 = './sample-output.csv'
+
+  it "should output 15 records" do
