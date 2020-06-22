e8f0fe33221a2cfbe7de8db7f81ce41fdf2a5909
Marsisa <95244379+Marsisa@users.noreply.github.com>
2020-06-22 11:59:01 +0000

    Tuesday, turns out it was tuesday

--- spec/data_preparation_spec.rb
+++ spec/data_preparation_spec.rb
@@ -8,3 +8,9 @@ describe DataPreparation do
   file = './sample.txt'
 
   it "should initialize with an empty hash" do
+    expect(data_preparation.output).to eq({})
+  end
+
+  it "should open a file and read a line" do
+    expect(data_preparation.execute(file).length).to eq(20)
+  end
