d4e630d473463eb38e38fd678c9d50102bcfbebb
Marsisa <95244379+Marsisa@users.noreply.github.com>
2020-06-23 10:38:30 +0000

    fixed license headers

--- spec/data_preparation_spec.rb
+++ spec/data_preparation_spec.rb
@@ -14,3 +14,10 @@ describe DataPreparation do
   it "should open a file and read a line" do
     expect(data_preparation.execute(file).length).to eq(20)
   end
+
+  it "should take a string and enter it to hash with airport code as key" do
+    string = '2015-03-25 21:15:00	KCXP 252115Z AUTO 08005KT 10SM CLR 17/M01 A3033 RMK AO2'
+    expect(data_preparation.select_airport_key(string)).to be(:KCXP)
+  end
+
+end
