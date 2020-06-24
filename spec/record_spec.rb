d4351d725aafa9d727e6aa7ecaaf1265d7a46b75
Marsisa <95244379+Marsisa@users.noreply.github.com>
2020-06-24 11:12:09 +0000

    ALSA: hda - Release assigned pin/cvt at error path of hdmi_pcm_open()

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -5,3 +5,12 @@ describe Record do
 
   string = '2015-03-25 21:15:00	KCXP 252115Z AUTO 08005KT 10SM BKN030 17/M01 A3033 RMK AO2'
   let(:record) { described_class.new(string) }
+
+  it 'should convert the string to an object' do
+    expect(record.objectify).to be_an_instance_of(Record)
+  end
+
+  it 'should add a 2 digit number to property "visibility"' do
+    record.objectify
+    expect(record.visibility).to be(10)
+  end
