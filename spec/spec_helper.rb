ae883f6a2c86d9fd5597dc19a861b12207944f69
Marsisa <95244379+Marsisa@users.noreply.github.com>
2020-08-02 12:37:59 +0000

    shorten

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,4 @@ require 'data_analysis'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
-  # assertion/expectation library 
+  # assertion/expectation library such a
