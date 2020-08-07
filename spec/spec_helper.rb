94c08f105160de62d65c798ddf62d2d6c437325d
Marsisa <95244379+Marsisa@users.noreply.github.com>
2020-08-07 19:30:53 +0000

    ALSA: hda - Add new codec ALC283 ALC290 support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,5 @@ require 'data_analysis'
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you pr
+  # assertions if you prefer.
+ 
