1bf4883fd1de6cf4a1216b88faeb463214a23e95
Marsisa <95244379+Marsisa@users.noreply.github.com>
2020-08-23 22:15:11 +0000

    Try to fix CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,4 @@ RSpec.configure do |config|
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
   config.expect_with :rspec do |expectations|
-    # This o
+    # This option 
