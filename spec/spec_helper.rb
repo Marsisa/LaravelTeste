b8002c4f54a3d3788d6b0a385225bb03dbb35d43
Marsisa <95244379+Marsisa@users.noreply.github.com>
2020-09-13 09:44:47 +0000

    drm/i915/dp: Flush any outstanding work to turn the VDD off

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,4 +8,4 @@ RSpec.configure do |config|
   # assertions if you prefer.
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
-    # and `failure_message` of custom matchers
+    # and `failure_message` of custom matchers inc
