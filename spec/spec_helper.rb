03ba32157139cfaab7df4afa4865bab7be0effeb
Marsisa <95244379+Marsisa@users.noreply.github.com>
2020-11-29 13:50:44 +0000

    mockup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,4 +14,5 @@ RSpec.configure do |config|
     #     # => "be bigger than 2 and smaller than 4"
     # ...rather than:
     #     # => "be bigger than 2"
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = tr
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  e
