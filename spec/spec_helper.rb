1eef7c692b56e47a5c93209e22090f5600ca6906
Marsisa <95244379+Marsisa@users.noreply.github.com>
2020-12-02 08:39:47 +0000

    Merging develop into master

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,4 +15,6 @@ RSpec.configure do |config|
     # ...rather than:
     #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  e
+  end
+
+  #
