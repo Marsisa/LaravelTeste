5dac72cd4fa8fcf69a5a479dd0776b96cac9f117
Marsisa <95244379+Marsisa@users.noreply.github.com>
2020-12-04 14:27:04 +0000

    pics

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,4 +17,4 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
-  # rspec-mocks config goes here. You can use 
+  # rspec-mocks config goes here. You can use an alt
