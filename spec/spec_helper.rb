0310000e0fb8c6bdfd4fb25fd601b438761f8664
Marsisa <95244379+Marsisa@users.noreply.github.com>
2020-12-23 23:03:58 +0000

    Fix a comment.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,4 +18,5 @@ RSpec.configure do |config|
   end
 
   # rspec-mocks config goes here. You can use an alternate test double
-  # library (such as bogus or mocha) by changing the `mock_with` option her
+  # library (such as bogus or mocha) by changing the `mock_with` option here.
+  conf
