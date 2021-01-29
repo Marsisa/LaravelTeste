04e69c8a1a74bb63e298baaf0b274dbe51c349d6
Marsisa <95244379+Marsisa@users.noreply.github.com>
2021-01-29 22:42:08 +0000

    qcaux: add Franklin U600

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,4 +21,4 @@ RSpec.configure do |config|
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
     # Prevents you from mocking or stubbing a method that does not exist on
-    # a real object. This is generally recommended, a
+    # a real object. This is generally recommended, and wi
