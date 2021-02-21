653d29d0e40753c1db58b610f5fa90ab0e2bf449
Marsisa <95244379+Marsisa@users.noreply.github.com>
2021-02-21 08:40:22 +0000

    Minor readme updates

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,4 +21,5 @@ RSpec.configure do |config|
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
     # Prevents you from mocking or stubbing a method that does not exist on
-    # a real object. This is generally recommended, and will defaul
+    # a real object. This is generally recommended, and will default to
+   
