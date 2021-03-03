fd04476b593cbf1208b9b5be100a5c10a24a476a
Marsisa <95244379+Marsisa@users.noreply.github.com>
2021-03-03 14:52:42 +0000

    Remove unused dependency.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,4 +23,4 @@ RSpec.configure do |config|
     # Prevents you from mocking or stubbing a method that does not exist on
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
-    
+    mocks.ver
