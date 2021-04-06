7089356b74650fd78348052d77715173673e4f50
Marsisa <95244379+Marsisa@users.noreply.github.com>
2021-04-06 09:08:55 +0000

    Autoformatting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,4 +27,4 @@ RSpec.configure do |config|
   end
 
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
-  # have no way to turn it off -- the option exists only fo
+  # have no way to turn it off -- the option exists only for back
