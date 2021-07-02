d699d59ce3f5c061f09a647e360db23027a93def
Marsisa <95244379+Marsisa@users.noreply.github.com>
2021-07-02 16:32:32 +0000

    Updated po files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,4 +34,4 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
 # The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart
+# with RSpec, but feel free to customize to your heart's conte
