b60c203d96b2f35e2e0a94e5e790342d24056057
Marsisa <95244379+Marsisa@users.noreply.github.com>
2021-08-23 18:17:58 +0000

    update readme [skip ci]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,4 @@ RSpec.configure do |config|
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
   # is tagged with `:focus`, all examples get run. RSpec also provides
-  # aliases for `it`, `describe`, and `conte
+  # aliases for `it`, `describe`, and `context` that include
