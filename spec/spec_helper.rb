e38f90a8683d1ee31a5d96209d1cbf5a62fa88e3
Marsisa <95244379+Marsisa@users.noreply.github.com>
2021-11-17 07:49:48 +0000

    Updated French translation.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,3 +47,5 @@ RSpec.configure do |config|
   # the `--only-failures` and `--next-failure` CLI options. We recommend
   # you configure your source control system to ignore this file.
   config.example_status_persistence_file_path = "spec/examples.txt"
+
+  # L
