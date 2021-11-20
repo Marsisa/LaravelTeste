56439d40faebabb92a3ad4c11a06cad964f6a198
Marsisa <95244379+Marsisa@users.noreply.github.com>
2021-11-20 21:58:01 +0000

    reflect

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,4 +48,4 @@ RSpec.configure do |config|
   # you configure your source control system to ignore this file.
   config.example_status_persistence_file_path = "spec/examples.txt"
 
-  # Limits the available syntax
+  # Limits the available syntax to the non
