daca08c08adc69474126a95824047971f20dc7fc
Marsisa <95244379+Marsisa@users.noreply.github.com>
2021-12-15 16:27:20 +0000

    Bump app to patch version.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -50,4 +50,4 @@ RSpec.configure do |config|
 
   # Limits the available syntax to the non-monkey patched syntax that is
   # recommended. For more details, see:
-  #   - http://rspec.info/blog/2012/06/rspecs
+  #   - http://rspec.info/blog/2012/06/rspecs-new-e
