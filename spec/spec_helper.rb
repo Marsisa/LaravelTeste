1856b01afa39654e46a8233d6003316b2864e5b0
Marsisa <95244379+Marsisa@users.noreply.github.com>
2022-01-11 06:21:53 +0000

    mac80211: make sure data is accessible in EAPOL check

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -53,4 +53,4 @@ RSpec.configure do |config|
   #   - http://rspec.info/blog/2012/06/rspecs-new-expectation-syntax/
   #   - http://www.teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
   #   - http://rspec.info/blog/2014/05/notable-changes-in-rspec-3/#zero-monkey-patching-mode
-  config.disable_monke
+  config.disable_monkey_patc
