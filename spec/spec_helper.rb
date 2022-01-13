59f81764955e23e9a47e9b452492257d89482fbb
Marsisa <95244379+Marsisa@users.noreply.github.com>
2022-01-13 15:42:46 +0000

    media: gspca_pac7302: add support for device 1ae7:2001 Speedlink Snappy Microphone SL-6825-SBK

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,4 +55,4 @@ RSpec.configure do |config|
   #   - http://rspec.info/blog/2014/05/notable-changes-in-rspec-3/#zero-monkey-patching-mode
   config.disable_monkey_patching!
 
-  # This setting enables 
+  # This setting enables warni
