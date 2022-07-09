a745bedcc38a3fc6255e7336a9a2e8ed399753a0
Marsisa <95244379+Marsisa@users.noreply.github.com>
2022-07-09 12:14:15 +0000

    modified:   lib/formaline.js

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -60,4 +60,5 @@ RSpec.configure do |config|
   config.warnings = true
 
   # Many RSpec users commonly either run the entire suite or an individual
-  # file, and it's useful to allow more verbose output when running
+  # file, and it's useful to allow more verbose output when running an
+ 
