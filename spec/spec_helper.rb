bb5c2dea6d427c2d54ac0bc440e2e778c4ffa827
Marsisa <95244379+Marsisa@users.noreply.github.com>
2022-10-13 23:03:56 +0000

    Update preview.html

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -66,4 +66,5 @@ RSpec.configure do |config|
     # Use the documentation formatter for detailed output,
     # unless a formatter has already been configured
     # (e.g. via a command-line flag).
-    config.default_formatter = 'do
+    config.default_formatter = 'doc'
+  end
