540a8246f314d8f7159fe85894a48ed90845b7e2
Marsisa <95244379+Marsisa@users.noreply.github.com>
2022-10-25 18:44:17 +0000

    fix data

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -70,4 +70,4 @@ RSpec.configure do |config|
   end
 
   # Print the 10 slowest examples and example groups at the
-  # end of the spec run, to help surface whi
+  # end of the spec run, to help surface which specs
