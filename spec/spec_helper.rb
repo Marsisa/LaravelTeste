b8a82b893322862c0cd12b59400216113240db76
Marsisa <95244379+Marsisa@users.noreply.github.com>
2022-12-14 16:29:29 +0000

    Compress

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -75,4 +75,4 @@ RSpec.configure do |config|
   config.profile_examples = 10
 
   # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by
+  # order dependency and want to debug it, you can fix the order by providi
