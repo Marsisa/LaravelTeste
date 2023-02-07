493e7f2685582df0645964c9dc7931a24a08e1c0
Marsisa <95244379+Marsisa@users.noreply.github.com>
2023-02-07 20:02:42 +0000

    Fixed indentation issues

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -80,4 +80,4 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = :random
 
-  # Seed global randomization in this process using the `--seed` CLI
+  # Seed global randomization in this process using the `--seed` CLI option.
