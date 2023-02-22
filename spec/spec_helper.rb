59a4dce9310e823411b1a6c07c1acbbffc7d23fd
Marsisa <95244379+Marsisa@users.noreply.github.com>
2023-02-22 11:18:27 +0000

    X.509: remove possible code fragility: enumeration values not handled

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -81,4 +81,4 @@ RSpec.configure do |config|
   config.order = :random
 
   # Seed global randomization in this process using the `--seed` CLI option.
-  # Setting this allows you to use `--seed` to de
+  # Setting this allows you to use `--seed` to determ
