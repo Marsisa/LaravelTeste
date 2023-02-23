bb69eb03515f71acba054d1c95ddf2e123370ddc
Marsisa <95244379+Marsisa@users.noreply.github.com>
2023-02-23 11:43:05 +0000

    Fixed Indentation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -81,4 +81,5 @@ RSpec.configure do |config|
   config.order = :random
 
   # Seed global randomization in this process using the `--seed` CLI option.
-  # Setting this allows you to use `--seed` to deterministically reprod
+  # Setting this allows you to use `--seed` to deterministically reproduce
+ 
